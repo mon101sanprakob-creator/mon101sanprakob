be08d727ed2082801ee83bd413e121487a31f6a5
mon101sanprakob-creator <mon101sanprakob@gmail.com>
2026-07-07 03:42:14 +0700

    Update config.py

--- engine/config.py
+++ engine/config.py
@@ -1,1 +1,49 @@
+"""
+###############################################################
+#
+# CONFIGURATION
+#
+###############################################################
+"""
 
+SCREEN_WIDTH = 1920
+
+SCREEN_HEIGHT = 1080
+
+MAX_HISTORY = 10000
+
+AUTO_SAVE = True
+
+USE_SOUND = True
+
+USE_ANIMATION = True
+
+USE_CACHE = True
+
+THEME = "SYNAPSE"
+
+LANGUAGE = "TH"
+
+DEFAULT_ENERGY = 50
+
+DEFAULT_FREQUENCY = 432
+
+BACKGROUND = "SPACE"
+
+LOGO = "logo1.png"
+
+DATABASE = "history.db"
+
+EXPORT_FOLDER = "export"
+
+CACHE_FOLDER = "cache"
+
+REPORT_FOLDER = "reports"
+
+ICON_SIZE = 64
+
+CHART_HEIGHT = 500
+
+CHART_WIDTH = 1000
+
+MAX_DECIMAL = 6
