7de27504430568b8a002fe4b594958f4e82a685d
mon101sanprakob-creator <mon101sanprakob@gmail.com>
2026-07-07 03:44:52 +0700

    Update version.py

--- engine/version.py
+++ engine/version.py
@@ -1,1 +1,68 @@
+"""
+###############################################################
+#
+# SYNAPSE ENGINE
+# VERSION INFORMATION
+#
+###############################################################
+"""
 
+APP_NAME = "SYNAPSE"
+
+APP_VERSION = "1.0.0"
+
+ENGINE_VERSION = "1.0"
+
+AUTHOR = "Mon101sanprakob"
+
+SLOGAN = "อยู่นิ้งๆไม่เจ็บตัว"
+
+DESCRIPTION = """
+SYNAPSE
+Sound & Visual Therapy
+
+Mathematical Analysis Engine
+Golden Ratio
+Moon Cycle
+Calendar
+Lunar Phase
+Energy Index
+
+"""
+
+COPYRIGHT = "Copyright © SYNAPSE"
+
+BUILD = "001"
+
+STATUS = "Development"
+
+DEBUG = True
+
+
+def version():
+
+    return f"{APP_NAME} {APP_VERSION}"
+
+
+def engine():
+
+    return ENGINE_VERSION
+
+
+def info():
+
+    return {
+
+        "app":APP_NAME,
+
+        "version":APP_VERSION,
+
+        "engine":ENGINE_VERSION,
+
+        "author":AUTHOR,
+
+        "status":STATUS,
+
+        "build":BUILD
+
+}
