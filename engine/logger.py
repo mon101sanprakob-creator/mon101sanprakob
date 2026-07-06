dfb8eb53600b017388104ae6b8a2dd08831f4a8b
mon101sanprakob-creator <mon101sanprakob@gmail.com>
2026-07-07 03:47:24 +0700

    Update logger.py

--- engine/logger.py
+++ engine/logger.py
@@ -1,1 +1,32 @@
+"""
+###############################################################
+#
+# LOGGER
+#
+###############################################################
+"""
 
+from datetime import datetime
+
+LOG = []
+
+
+def write(message):
+
+    now = datetime.now()
+
+    text = f"[{now}] {message}"
+
+    LOG.append(text)
+
+    return text
+
+
+def history():
+
+    return LOG
+
+
+def clear():
+
+    LOG.clear()
