be5c077ac8c969fd2dd5a0d625ffb99b46bfbf05
mon101sanprakob-creator <mon101sanprakob@gmail.com>
2026-07-09 05:19:00 +0700

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,5 +1,1 @@
-import streamlit as st
-from engine.constants import ENTERTAINMENT_DISCLAIMER, COLOR_PALETTE
 
-# แสดงข้อความแจ้งเตือนความบันเทิงที่หน้าแรกตามหลักเกณฑ์
-st.info(ENTERTAINMENT_DISCLAIMER)
