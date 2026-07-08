5f95d4b8172246eabaefa438819cec9006a3867d
mon101sanprakob-creator <mon101sanprakob@gmail.com>
2026-07-09 05:19:35 +0700

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,1 +1,6 @@
 
+# engine/__init__.py
+
+# ใช้ . เพื่อบอกว่าเป็นไฟล์ที่อยู่ในโฟลเดอร์เดียวกัน (Relative Import)
+from .constants import ENTERTAINMENT_DISCLAIMER, COLOR_PALETTE
+from .synapse_engine import SynapseEngine
